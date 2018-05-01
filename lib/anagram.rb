72b66c80b68befb8f7998735bccf9272478d2548
Emily Jirles <emily.jirles@gmail.com>
2018-05-01 12:55:41 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(anagram_array) 
+    word = @word.split('').sort 
+    anagram_array.each do |anagram|
+      if anagram.split('').sort == word 
+        result = word 
+      end
+    end
+    word 
+  end
+  
+end
